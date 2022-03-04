09b266b071364398d3379d085e246c5ffbe2d173
Vivek Patel <vivekpatel1999.vdp@gmail.com>
2022-03-04 12:25:07 -0500

    Created container and student dashboard skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <StudentDashboard :scores=scores :past_quizzes=past_quizzes></StudentDashboard>
+    <QuizzesContainer :quizzes=past_quizzes></QuizzesContainer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StudentDashboard from './components/StudentDashboard.vue'
+import QuizzesContainer from './components/QuizzesContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StudentDashboard,
+    QuizzesContainer
+  },
+  data () {
+    return {
+      scores: [
+        {
+          qid: 1,
+          score: 100
+        },
+        {
+          qid: 2,
+          score: 70
+        },
+        {
+          qid: 3,
+          score: 90
+        }
+      ],
+      past_quizzes: [
+        {
+          qid: 1,
+          name: "Unit 1"
+        },
+        {
+          qid: 2,
+          name: "Unit 2"
+        },
+        {
+          qid: 3,
+          name: "Unit 3"
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  padding-left:10%;
+  padding-right:10%;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: linear-gradient(172.4deg, #24A3FF 5.89%, #24A3FF 5.9%, #0038FF 91.52%);
+}
+
+.container{
+  background-color: linear-gradient(172.4deg, #24A3FF 5.89%, #24A3FF 5.9%, #0038FF 91.52%);
 }
 </style>
---
+++ src/components/StudentDashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="student">
+    <h1>{{ "Welcome <user>. Here is how you have done in your past quizzes:" }}</h1>
+    <div v-for="(item, index) in scores" :key="item.qid">
+        <li class=student__score__section>{{ past_quizzes[index].name}}: <span class=student__score>{{ item.score}}</span></li>     
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'StudentDashboard',
+  props: {
+    scores: Array,
+    past_quizzes: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .student{
+    color:black;
+  }
+  .student__score__section {
+    font-size:1.5rem;
+  }
+  .student__score{
+    font-weight: 700;
+  }
+  
+</style>
