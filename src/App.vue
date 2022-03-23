f6a3f2243bace24c0cf71c57d505c1dc2a1265e4
Anna Beaver <aelisebe@live.unc.edu>
2022-03-22 21:00:16 -0400

    created roster page and file for global variables

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <StudentRoster :students="this.$store.state.students"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import StudentRoster from './components/Roster.vue';
+//import studentData from './components/studentData.js'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StudentRoster,
+  
   }
 }
 </script>
@@ -21,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
