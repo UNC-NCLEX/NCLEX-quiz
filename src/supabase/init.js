056f75ec773811e29455830c9be1cc725832a309
Vivek Patel <vivekpatel1999.vdp@gmail.com>
2022-04-04 17:04:58 -0400

    supabase implemented

--- src/supabase/init.js
+++ src/supabase/init.js
@@ -1,10 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Provide a custom schema. Defaults to "public".
-const supabase = createClient(
-  "https://peqgyvfxrmrwokruwopr.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlcWd5dmZ4cm1yd29rcnV3b3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDUwNjM5MTQsImV4cCI6MTk2MDYzOTkxNH0.KMerQLrsV2uzbfTPlafw-lEdpk4BMfQRK8UUSgav0hc",
-  {
-    schema: "other_schema",
-  }
-);
+
+const supabaseUrl = process.env.VUE_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.VUE_APP_SUPABASE_ANON_KEY;
+console.log(supabaseUrl);
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
