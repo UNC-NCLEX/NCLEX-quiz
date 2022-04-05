7f5f315378f693424871fa5b357b6fe9494da0a1
Vivek Patel <vivekpatel1999.vdp@gmail.com>
2022-04-05 16:00:39 -0400

    cleared console log

--- src/supabase/init.js
+++ src/supabase/init.js
@@ -4,6 +4,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.VUE_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.VUE_APP_SUPABASE_ANON_KEY;
-console.log(supabaseUrl);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
